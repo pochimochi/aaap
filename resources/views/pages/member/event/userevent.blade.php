877b405103fda4ab9d655fced3f866f8386822eb
Carlos <cpbasco13@gmail.com>
2018-10-05 11:55:56 +0800

    Reborn Thesis hahahaha

--- resources/views/pages/member/event/userevent.blade.php
+++ resources/views/pages/member/event/userevent.blade.php
@@ -4,43 +4,43 @@
     <div class="container">
 
 
+        <div class="table-responsive">
+            <table id="myTable" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th>Event</th>
 
-                <div class="table-responsive">
-                    <table id="myTable" class="table table-bordered table-striped">
-                        <thead>
-                        <tr>
-                            <th>Event</th>
+                </tr>
+                </thead>
+                <tbody>
 
-                        </tr>
-                        </thead>
-                        <tbody>
-
-                        @foreach ($events as $event)
-                            <tr>
-
+                @foreach ($events as $event)
+                    <tr>
 
 
-    <td><div class="box box-shadow">{{ $event->eventName }}<br>
-        {{ $event->eventDescription }}<br>
-        {{ $event->postedBy }}
-        {{ $event->modifiedBy }}<br>
-        {{ $event->isPaid== 1 ? 'General' : 'Special'}}<br>
-        {{ $event->status== 1 ? 'Active' : 'Inactive'}}
-        {{ $event->endDate }}<br>
-        <a href="{{URL::to('/userjoin', $event->eventId)}}"
-           class="btn btn-warning btn-rounded">Join</a></div></td>
+                        <td>
+                            <div class="box box-shadow">{{ $event->eventName }}<br>
+                                {{ $event->eventDescription }}<br>
+                                {{ $event->postedBy }}
+                                {{ $event->modifiedBy }}<br>
+                                {{ $event->isPaid== 1 ? 'General' : 'Special'}}<br>
+                                {{ $event->status== 1 ? 'Active' : 'Inactive'}}
+                                {{ $event->endDate }}<br>
+                                <a href="{{URL::to('/userjoin', $event->eventId)}}"
+                                   class="btn btn-warning btn-rounded">Join</a></div>
+                        </td>
 
 
-                                {{--<td><a href="{{URL::to('/eventdelete', $event->eventId)}}"
-                                class="btn btn-warning btn-rounded">Delete</a></td>--}}
-                            </tr>
-                        @endforeach
+                        {{--<td><a href="{{URL::to('/eventdelete', $event->eventId)}}"
+                        class="btn btn-warning btn-rounded">Delete</a></td>--}}
+                    </tr>
+                @endforeach
 
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                </tbody>
+            </table>
         </div>
+    </div>
+    </div>
     </div>
 
 
